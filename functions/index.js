f62c8ad700d08bcbea7febc70ef5c06b097f551f
Hrushikesh Nahak <hnayak7@gmail.com>
2021-12-06 23:39:14 +0530

    scheduler

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 const express = require('express');
 const cors = require('cors');
 const { FBAuth } = require('./utils/fbAuthMiddleware');
+// require('./utils/schedulers');
 const {
     userSignin,
     userSignup,
---
+++ functions/utils/schedulers.js
@@ -0,0 +1,13 @@
+const cron = require('node-cron');
+
+// cron.schedule('* * * * *', () =>  {
+//     console.log('Task 1 will execute every min until stopped');
+// });
+
+// const task2 = cron.schedule('* * * * *', () =>  {
+//     console.log('Run Task2 every 10s');
+// });
+
+// module.exports = {
+//     task1
+// }
